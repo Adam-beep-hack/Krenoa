72eae15b1fe4b668a5dd4888b6e8b30820ae38c0
Adam-beep-hack <hamaadama1er@gmail.com>
2026-09-05 11:34:51 +0100

    Update EcranSplash.kt

--- app/src/main/java/com/krenoa/app/ui/EcranSplash.kt
+++ app/src/main/java/com/krenoa/app/ui/EcranSplash.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
